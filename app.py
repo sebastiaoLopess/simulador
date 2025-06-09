ecd16bbee04b10073698dfecf4b83bb6f134e0c7
sebastiao lopes <lopescastroneto@gmail.com>
2025-06-08 21:09:55 -0300

    atualizado versao

--- app.py
+++ app.py
@@ -16,6 +16,25 @@ st.markdown(
         border-radius: 8px;
         padding: 0.5em 2em;
     }
+    .card {
+        background-color: white;
+        border-radius: 16px;
+        padding: 30px;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+        margin-top: 30px;
+        text-align: center;
+    }
+    .card h2 {
+        font-size: 26px;
+        margin-bottom: 10px;
+        color: #333;
+    }
+    .card .valor-destaque {
+        font-size: 42px;
+        font-weight: 800;
+        color: #007bff;
+        margin-top: 10px;
+    }
     </style>
     """,
     unsafe_allow_html=True
@@ -39,11 +58,30 @@ parcela_seguro = total_final / prazo
 parcela_sem_seguro = total / prazo
 lance = total_final * (lance_embutido/100)
 
+
 plano = st.selectbox("Plano", ["Plano Integral", "Plano Light"])
 
 # Botão
 if st.button("Gerar"):
+    qtd_parcelas_restante = int(prazo - 1 -(lance / parcela_seguro))
     # Aqui você pode adicionar seus cálculos com base nos inputs
-    st.success(f"Cálculos gerados com sucesso! {credito_final}")
+
+    valor_formatado = f"R$ {credito_final:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+    parcela_formatado = f"R$ {parcela_seguro:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+
+    st.markdown(
+    f"""
+        <div class="card">
+            <h3>Crédito a receber</h3>
+            <div class="valor-destaque">{valor_formatado}</div>
+            <h3>Valor da parcela</h3>
+            <div class="valor-destaque">{parcela_formatado}</div>
+            <h3>Parcelas Restantes</h3>
+            <div class="valor-destaque">{qtd_parcelas_restante}</div>
+        </div>
+    """,
+    unsafe_allow_html=True
+    )
     # Exemplo:
     st.write("🧮 Você pode exibir os resultados dos cálculos aqui.")
+
